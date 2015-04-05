5619f8de14469620e960e1765ae05bbccf145704
sachinpk <sachinpray4me@gmail.com>
2015-04-05 08:08:28 -0700

    send mail post request code

---
+++ admin/send_email.php
@@ -0,0 +1,17 @@
+<?php
+error_reporting(E_ALL ^ E_DEPRECATED);
+include("project connection.php");
+session_start();
+if(isset($_POST['finishbutton']))
+{
+$to=$_POST['email_to'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
+mysql_query("INSERT INTO `mail`(`from`, `to`, `subject`, `message`) VALUES ('admin','$to','$subject','$message')");
+
+// header("Location:admin.php");
+echo $to;
+}
+
+
+?>
