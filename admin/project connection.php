ba0d7089d706830b9129dd2cc706e04d241a5bab
sachinpk <sachinpray4me@gmail.com>
2015-04-11 04:14:31 -0700

    removed  unwanted query

--- admin/project connection.php
+++ admin/project connection.php
@@ -6,7 +6,6 @@ if(!$con)
 die("SERVER NOT FOUND".mysql_error());
 }
 mysql_select_db('fnp',$con)or die("DATABASE NOT FOUND".mysql_error());
-$result = mysql_query("SELECT * FROM `mail` WHERE `to`='admin'");
 $undread=mysql_query("SELECT count(`read`) AS unread FROM `mail` WHERE `to`='admin' AND `read`=0");
 $count=mysql_fetch_array($undread);
 $unreadcount=$count['unread'];
